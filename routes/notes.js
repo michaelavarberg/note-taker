5f6ab617699f4b9149f3804ae555e0293efbeb41
Michaela Varberg <michaelavarberg@gmail.com>
2022-09-06 15:12:03 -0600

    fixed bugs on POST and GET routes

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const notes = require("express").Router();
+const { uuid } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+
+//GET route
+notes.get("/", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+//POST route
+notes.post("/", (req, res) => {
+  const { title, content } = req.body;
+
+  if (title && content) {
+    const newNote = {
+      title,
+      content,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      state: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting note.");
+  }
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 //imports
 const express = require("express");
-const { appendFile } = require("fs"); // where did this come from??
+const { readFromFile, readAndAppend } = require("./helpers/fsUtils");
 const path = require("path");
+// const api = require("./routes/notes.js");
+const uuid = require("./helpers/uuid");
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use("/api", api);
 app.use(express.static("public"));
-
-//GET Route for homepage
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
 
 //GET Route for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//POST Route for api/notes
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      state: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting note.");
+  }
+});
+
+//GET Route for api/notes
+app.get("/api/notes", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+//GET Route for homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
